93606072736ba0c8372cb141836b27bc67507bdf
Documenter.jl <documenter@juliadocs.github.io>
2024-12-06 12:17:52 +0000

    build based on dcc22d2

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = VP4Optim","category":"page"},{"location":"#VP4Optim","page":"Home","title":"VP4Optim","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for VP4Optim.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [VP4Optim]","category":"page"}]
+}
